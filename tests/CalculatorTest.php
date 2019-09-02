732bd8af9aa9fe41817f88da853657d41f7f3653
theoAlex <alex.theo@gmx.de>
2019-09-02 16:03:50 +0200

    remove index.php

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -9,26 +9,29 @@ use PHPUnit\Framework\Error\Error;
 
 final class CalculatorTest extends TestCase
 {
-    
+    protected $myCalc;
+
+    protected function setup(): void
+    {
+        $this->myCalc = new Calculator();
+    }
+
     public function testCalculatesSumFromTwoNrs(): void
     {
-        $myCalc = new Calculator();
-        $this->assertSame(2, $myCalc->calculateSum(1, 1));
-        $this->assertSame(3, $myCalc->calculateSum(1, 2));
+        $this->assertSame(2, $this->myCalc->calculateSum(1, 1));
+        $this->assertSame(3, $this->myCalc->calculateSum(1, 2));
     }
     
     public function testCalculatesSumFromAVariableAmountOfNrs(): void
     {
-        $myCalc = new Calculator();
-        $this->assertSame(3, $myCalc->calculateSum(1, 1, 1));
-        $this->assertSame(21, $myCalc->calculateSum(1, 2, 3, 4, 5, 6));
+        $this->assertSame(3, $this->myCalc->calculateSum(1, 1, 1));
+        $this->assertSame(21, $this->myCalc->calculateSum(1, 2, 3, 4, 5, 6));
     }
 
     public function testAcceptsOnlyNrsAsArgumentTypes(): void
     {
-        $myCalc = new Calculator();
-        $this->assertSame(2.2, $myCalc->calculateSum(1, 1.2));
+        $this->assertSame(2.2, $this->myCalc->calculateSum(1, 1.2));
         $this->expectException(\InvalidArgumentException::class);
-        $myCalc->calculateSum('1', true, [1, 2], $myCalc, null);
+        $this->myCalc->calculateSum('1', true, [1, 2], $this->myCalc, null);
     }
 }
